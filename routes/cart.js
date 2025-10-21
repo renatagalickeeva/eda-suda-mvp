79c92942a200c8b43bd12635f8fb131c216cb9f4
renat <renat@KIRIKIRI>
2025-10-21 19:04:26 +0500

    fix: correct template name in cart route from 'cartt' to 'cat'

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('cart');
+  res.render('cartt');
 });
 
 module.exports = router;
